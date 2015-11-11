74cb2051dd10e24c48f9e08e70208590827c56f1
a.mikhalev <a.mikhalev@pflb.ru>
2015-11-11 18:56:21 +0300

    Add code for generating start & stop batch files

--- generate.bat
+++ generate.bat
@@ -16,9 +16,14 @@ if [%2] == [] (
 
 set /a durationSeconds=%durationHours%*3600
 
-md %cd%\%durationHours%h
+md %cd%\%durationHours%h 
 
 setlocal enabledelayedexpansion
+
+set startFile=%cd%\%durationHours%h_start.bat
+echo. > %startFile%
+set stopFile=%cd%\%durationHours%h_stop.bat
+echo. > %stopFile%
 
 set file[0]=Application.Memory
 set file[1]=Application.NetworkInterface
@@ -42,6 +47,12 @@ for /l %%n in (0,1,5) do (
 	
 	logman delete %durationHours%h.!file[%%n]! > nul
 	logman import -name %durationHours%h.!file[%%n]! -xml !outputFile!
+	
+	echo logman start %durationHours%h.!file[%%n]! >> !startFile!
+	echo logman stop %durationHours%h.!file[%%n]! >> !stopFile!
 )
+
+
+
 
 endlocal
