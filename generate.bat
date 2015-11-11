246b589ba0eee930272eec63fdfd4cfeca518add
a.mikhalev <a.mikhalev@pflb.ru>
2015-11-11 18:36:04 +0300

    Fix logic errors

--- generate.bat
+++ generate.bat
@@ -3,6 +3,17 @@
 set durationHours=%1
 set sampleInterval=%2
 
+if [%1] == [] (
+	echo Please rerun script with 2 parameters: duration ^(hours^) and sample interval ^(seconds^).
+	pause
+	exit /B 1
+)
+if [%2] == [] (
+	echo Please rerun script with 2 parameters: duration ^(hours^) and sample interval ^(seconds^).
+	pause
+	exit /B 1
+)   
+
 set /a durationSeconds=%durationHours%*3600
 
 md %cd%\%durationHours%h
@@ -19,19 +30,17 @@ set file[5]=Application.System
 for /l %%n in (0,1,5) do (
 	
 	set outputFile=%durationHours%h\%durationHours%h.!file[%%n]!.xml
+	break > !outputFile!
 	echo !outputFile! created
 	
 	for /F "delims=" %%G in (Templates\!file[%%n]!.xml) do (
 		set LINE=%%G
-		if not "!LINE!" == "!LINE:Duration=!" (
-			set LINE=!LINE:#####=%durationSeconds%!
-		)
-		if not "!LINE!" == "!LINE:SampleInterval=!" (
-			set LINE=!LINE:#####=%sampleInterval%!
-		)
+		set LINE=!LINE:^<Duration^>#####^</Duration^>=^<Duration^>%durationSeconds%^</Duration^>!
+		set LINE=!LINE:^<SampleInterval^>#####^</SampleInterval^>=^<SampleInterval^>%sampleInterval%^</SampleInterval^>!
 		echo !LINE! >> !outputFile!
 	) 
 	
+	logman delete %durationHours%h.!file[%%n]! > nul
 	logman import -name %durationHours%h.!file[%%n]! -xml !outputFile!
 )
 
